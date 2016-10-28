325f6b23f529cf6e7214e3ff4c54800126540b99
Vincenzo Maffione <v.maffione@gmail.com>
2016-10-28 16:37:29 +0200

    virtio prodcons: producer: add argument parsing

--- virtio-pc-driver/producer.c
+++ virtio-pc-driver/producer.c
@@ -10,19 +10,47 @@
 #include "virtio-prodcons.h"
 
 
+static void
+usage(void)
+{
+	printf("producer [-w NANOSECONDS]\n");
+}
+
 int
 main(int argc, char **argv)
 {
-	int fd = open("/dev/virtio-pc", O_RDWR);
 	struct virtpc_ioctl vio;
+	int fd;
 	int ret;
+	int ch;
+	int x;
 
+	while ((ch = getopt(argc, argv, "hw:")) != -1) {
+		switch (ch) {
+		default:
+		case 'h':
+			usage();
+			return 0;
+
+		case 'w':
+			x = atoi(optarg);
+			if (x < 1) {
+				printf("Invalid -w option argument\n");
+				usage();
+				return 0;
+			}
+			vio.wp = (unsigned int)x;
+			break;
+		}
+	}
+
+	fd = open("/dev/virtio-pc", O_RDWR);
 	if (fd < 0) {
 		printf("open(virtio-pc) failed [%s]\n", strerror(errno));
 		return -1;
 	}
 
-	vio.wp = 150;
+	vio.wp = 150; /* in nanoseconds */
 	vio.devid = 0;
 	ret = ioctl(fd, 0, &vio);
 	if (ret < 0) {
