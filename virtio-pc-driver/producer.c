e5486c8b53b5ae000e98cd8fb6f7ed219ead498f
Vincenzo Maffione <v.maffione@gmail.com>
2016-11-04 16:02:32 +0100

    producer: reindent

--- virtio-pc-driver/producer.c
+++ virtio-pc-driver/producer.c
@@ -13,9 +13,9 @@
 static void
 usage(void)
 {
-	printf("producer [-p WP_NANOSEC] [-c WC_NANOSEC] "
-                        "[-P YP_NANOSEC] [-C YC_NANOSEC] "
-                        "[-d DURATION_SEC] [-s]\n");
+    printf("producer [-p WP_NANOSEC] [-c WC_NANOSEC] "
+            "[-P YP_NANOSEC] [-C YC_NANOSEC] "
+            "[-d DURATION_SEC] [-s]\n");
 }
 
 static unsigned int
@@ -36,65 +36,65 @@ parseuint(const char *s)
 int
 main(int argc, char **argv)
 {
-	struct virtpc_ioctl_data vio;
-	int fd;
-	int ret;
-	int ch;
+    struct virtpc_ioctl_data vio;
+    int fd;
+    int ret;
+    int ch;
 
-	vio.wp = 150; /* in nanoseconds */
-	vio.wc = 300; /* in nanoseconds */
-	vio.yp = 5000; /* in nanoseconds */
-	vio.yc = 5000; /* in nanoseconds */
-	vio.duration = 15; /* in seconds */
-        vio.sleeping = 0; /* producer don't use sleeping */
-	vio.devid = 0;
+    vio.wp = 150; /* in nanoseconds */
+    vio.wc = 300; /* in nanoseconds */
+    vio.yp = 5000; /* in nanoseconds */
+    vio.yc = 5000; /* in nanoseconds */
+    vio.duration = 15; /* in seconds */
+    vio.sleeping = 0; /* producer don't use sleeping */
+    vio.devid = 0;
 
-	while ((ch = getopt(argc, argv, "hsp:d:c:P:C:")) != -1) {
-		switch (ch) {
-		default:
-		case 'h':
-			usage();
-			return 0;
+    while ((ch = getopt(argc, argv, "hsd:p:c:P:C:")) != -1) {
+        switch (ch) {
+            default:
+            case 'h':
+                usage();
+                return 0;
 
-                case 's':
-                        vio.sleeping = 1;
-                        break;
+            case 's':
+                vio.sleeping = 1;
+                break;
 
-		case 'p':
-                        vio.wp = parseuint(optarg);
-			break;
+            case 'p':
+                vio.wp = parseuint(optarg);
+                break;
 
-		case 'c':
-			vio.wc = parseuint(optarg);
-			break;
+            case 'c':
+                vio.wc = parseuint(optarg);
+                break;
 
-		case 'P':
-                        vio.yp = parseuint(optarg);
-			break;
+            case 'P':
+                vio.yp = parseuint(optarg);
+                break;
 
-		case 'C':
-			vio.yc = parseuint(optarg);
-			break;
+            case 'C':
+                vio.yc = parseuint(optarg);
+                break;
 
-		case 'd':
-			vio.duration = parseuint(optarg);
-			break;
-		}
-	}
+            case 'd':
+                vio.duration = parseuint(optarg);
+                break;
+        }
+    }
 
-	fd = open("/dev/virtio-pc", O_RDWR);
-	if (fd < 0) {
-		printf("open(virtio-pc) failed [%s]\n", strerror(errno));
-		return -1;
-	}
+    fd = open("/dev/virtio-pc", O_RDWR);
+    if (fd < 0) {
+        printf("open(virtio-pc) failed [%s]\n", strerror(errno));
+        return -1;
+    }
 
-	ret = ioctl(fd, 0, &vio);
-	if (ret < 0 && errno != EAGAIN) {
-		printf("ioctl(virtio-pc) failed [%s]\n", strerror(errno));
-	}
+    ret = ioctl(fd, 0, &vio);
+    if (ret < 0 && errno != EAGAIN) {
+        printf("ioctl(virtio-pc) failed [%s]\n", strerror(errno));
+    }
 
-	close(fd);
+    close(fd);
 
-	return 0;
+    return 0;
 }
 
