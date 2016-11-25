7a220830a15f79d8d42f6b5c830d2621de60c417
Vincenzo Maffione <v.maffione@gmail.com>
2016-11-25 11:44:03 +0100

    threads: tp

--- virtio-pc-driver/threads/threads.c
+++ virtio-pc-driver/threads/threads.c
@@ -5,31 +5,56 @@
 #include <pthread.h>
 #include <errno.h>
 
+static struct global {
+    unsigned int wp;
+    unsigned int wc;
+    unsigned int yp;
+    unsigned int yc;
+    unsigned int psleeps;
+    unsigned int csleeps;
+} _g;
+
 
 static void *
 producer(void *opaque)
 {
+    struct global *g = opaque;
+
+    (void)g;
+
     return NULL;
 }
 
 static void *
 consumer(void *opaque)
 {
+    struct global *g = opaque;
+
+    (void)g;
+
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
+    struct global *g = &_g;
     pthread_t thp, thc;
     int ret;
 
-    ret = pthread_create(&thp, NULL, producer, NULL);
+    g->wp = 2100;
+    g->wc = 2000;
+    g->yp = 5000;
+    g->yc = 5000;
+    g->psleeps = 0;
+    g->csleeps = 0;
+
+    ret = pthread_create(&thp, NULL, producer, g);
     if (ret) {
         perror("pthread_create(P)");
         exit(EXIT_FAILURE);
     }
 
-    ret = pthread_create(&thc, NULL, consumer, NULL);
+    ret = pthread_create(&thc, NULL, consumer, g);
     if (ret) {
         perror("pthread_create(C)");
         exit(EXIT_FAILURE);
