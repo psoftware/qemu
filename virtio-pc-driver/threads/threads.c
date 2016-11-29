b8faa5889c4df6f089d5a0fe56bf8ac5a0c20bae
Vincenzo Maffione <v.maffione@gmail.com>
2016-11-29 14:26:30 +0100

    threads: P, C: skeleton

--- virtio-pc-driver/threads/threads.c
+++ virtio-pc-driver/threads/threads.c
@@ -5,7 +5,11 @@
 #include <pthread.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
+#include <sys/eventfd.h>
 
+
+/* QLEN must be a power of two */
 #define QLEN 128
 
 static struct global {
@@ -16,18 +20,59 @@ static struct global {
     unsigned int yc;
     unsigned int psleeps;
     unsigned int csleeps;
+    int pnotify;
+    int cnotify;
 
     uint64_t q[QLEN];
+    volatile unsigned int p;
+    volatile unsigned int ce;
+    uint64_t pnotifs;
+
+    volatile unsigned int c;
+    volatile unsigned int pe;
+    uint64_t pkts;
+    uint64_t cnotifs;
 } _g;
 
+static inline int
+queue_empty(struct global *g)
+{
+    return g->p == g->c;
+}
+
+static inline unsigned int
+queue_next(unsigned int idx)
+{
+    return (idx + 1) & (QLEN-1);
+}
+
+static inline int
+queue_full(struct global *g)
+{
+    return queue_next(g->p) == g->c;
+}
 
 static void *
 producer(void *opaque)
 {
     struct global *g = opaque;
+    int need_notify;
+    uint64_t x;
 
     while (!g->stop) {
-        usleep(10);
+        if (queue_full(g)) {
+            g->ce = g->c;
+            /* barrier and double-check */
+            read(g->cnotify, &x, sizeof(x));
+        }
+        usleep(g->wp);
+        need_notify = (g->p == g->pe);
+        g->p = queue_next(g->p);
+        if (need_notify) {
+            x = 1;
+            write(g->pnotify, &x, sizeof(x));
+            g->pnotifs ++;
+        }
     }
 
     return NULL;
@@ -37,9 +82,24 @@ static void *
 consumer(void *opaque)
 {
     struct global *g = opaque;
+    int need_notify;
+    uint64_t x;
 
     while (!g->stop) {
-        usleep(10);
+        if (queue_empty(g)) {
+            g->pe = g->p;
+            /* barrier and double-check */
+            read(g->pnotify, &x, sizeof(x));
+        }
+        usleep(g->wc);
+        need_notify = (g->c == g->ce);
+        g->c = queue_next(g->c);
+        if (need_notify) {
+            x = 1;
+            write(g->cnotify, &x, sizeof(x));
+            g->cnotifs ++;
+        }
+        g->pkts ++;
     }
 
     return NULL;
@@ -58,9 +118,19 @@ int main(int argc, char **argv)
     pthread_t thp, thc;
     int ret;
 
+    memset(g, 0, sizeof(*g));
+
+    g->pnotify = eventfd(0, 0);
+    g->cnotify = eventfd(0, 0);
+    if (g->pnotify < 0 || g->cnotify < 0) {
+        perror("eventfd()");
+        return -1;
+    }
+
     g->stop = 0;
-    g->wp = 2100;
-    g->wc = 2000;
+
+    g->wp = 2;
+    g->wc = 1;
     g->yp = 5000;
     g->yc = 5000;
     g->psleeps = 0;
