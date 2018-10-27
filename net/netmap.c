f735c6834471a831f9b69cd0722de490c0d0f025
Vincenzo Maffione <v.maffione@gmail.com>
2018-10-27 19:39:01 +0200

    add credits

--- net/netmap.c
+++ net/netmap.c
@@ -2,6 +2,7 @@
  * netmap access for qemu
  *
  * Copyright (c) 2012-2013 Luigi Rizzo
+ *               2013-2018 Vincenzo Maffione
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
